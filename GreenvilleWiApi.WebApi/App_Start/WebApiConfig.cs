560f9cef23c61e70e5bd08582281e4eb10a4ae46
Mike Rosack <mrosack@gmail.com>
2015-07-24 17:21:14 -0500

    return JSON by default

--- GreenvilleWiApi.WebApi/App_Start/WebApiConfig.cs
+++ GreenvilleWiApi.WebApi/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
@@ -24,6 +25,7 @@ namespace GreenvilleWiApi.WebApi
             var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
             jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
             jsonFormatter.SerializerSettings.Converters.Add(new StringEnumConverter { CamelCaseText = true });
+            jsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
