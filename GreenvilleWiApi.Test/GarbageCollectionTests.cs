1b685a97097970cddfa0664a604f951dfb381901
Mike Rosack <mrosack@gmail.com>
2015-10-13 18:41:18 -0500

    switch to xunit, entire project in vnext!

--- GreenvilleWiApi.Test/GarbageCollectionTests.cs
+++ GreenvilleWiApi.Test/GarbageCollectionTests.cs
@@ -1,31 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using GreenvilleWiApi.Data.GarbageCollection;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace GreenvilleWiApi.Test
 {
-    [TestClass]
     public class GarbageCollectionTests
     {
-        [TestMethod]
+        [Fact]
         public void CorrectGarbageCollectionTypesReturned()
         {
             // Test normal collection days
-            CollectionAssert.AreEqual(new List<GarbageCollectionType> { GarbageCollectionType.Trash }, GarbageDayCalculator.CollectionTypesForDay(new DateTime(2015, 7, 3), DayOfWeek.Friday));
-            CollectionAssert.AreEqual(new List<GarbageCollectionType> { GarbageCollectionType.Trash, GarbageCollectionType.Recycling }, GarbageDayCalculator.CollectionTypesForDay(new DateTime(2015, 7, 10), DayOfWeek.Friday));
+            Assert.Equal(new List<GarbageCollectionType> { GarbageCollectionType.Trash }, GarbageDayCalculator.CollectionTypesForDay(new DateTime(2015, 7, 3), DayOfWeek.Friday));
+            Assert.Equal(new List<GarbageCollectionType> { GarbageCollectionType.Trash, GarbageCollectionType.Recycling }, GarbageDayCalculator.CollectionTypesForDay(new DateTime(2015, 7, 10), DayOfWeek.Friday));
 
             // Test wrong collection days
-            CollectionAssert.AreEqual(new List<GarbageCollectionType> { }, GarbageDayCalculator.CollectionTypesForDay(new DateTime(2015, 7, 3), DayOfWeek.Wednesday));
-            CollectionAssert.AreEqual(new List<GarbageCollectionType> { }, GarbageDayCalculator.CollectionTypesForDay(new DateTime(2015, 7, 10), DayOfWeek.Wednesday));
+            Assert.Equal(new List<GarbageCollectionType> { }, GarbageDayCalculator.CollectionTypesForDay(new DateTime(2015, 7, 3), DayOfWeek.Wednesday));
+            Assert.Equal(new List<GarbageCollectionType> { }, GarbageDayCalculator.CollectionTypesForDay(new DateTime(2015, 7, 10), DayOfWeek.Wednesday));
 
             // Test holiday collection days
-            CollectionAssert.AreEqual(new List<GarbageCollectionType> { GarbageCollectionType.Trash, GarbageCollectionType.Recycling }, GarbageDayCalculator.CollectionTypesForDay(new DateTime(2015, 11, 25), DayOfWeek.Wednesday));
-            CollectionAssert.AreEqual(new List<GarbageCollectionType> { GarbageCollectionType.Trash, GarbageCollectionType.Recycling }, GarbageDayCalculator.CollectionTypesForDay(new DateTime(2015, 11, 28), DayOfWeek.Friday));
-            CollectionAssert.AreEqual(new List<GarbageCollectionType> { }, GarbageDayCalculator.CollectionTypesForDay(new DateTime(2015, 11, 27), DayOfWeek.Friday));
+            Assert.Equal(new List<GarbageCollectionType> { GarbageCollectionType.Trash, GarbageCollectionType.Recycling }, GarbageDayCalculator.CollectionTypesForDay(new DateTime(2015, 11, 25), DayOfWeek.Wednesday));
+            Assert.Equal(new List<GarbageCollectionType> { GarbageCollectionType.Trash, GarbageCollectionType.Recycling }, GarbageDayCalculator.CollectionTypesForDay(new DateTime(2015, 11, 28), DayOfWeek.Friday));
+            Assert.Equal(new List<GarbageCollectionType> { }, GarbageDayCalculator.CollectionTypesForDay(new DateTime(2015, 11, 27), DayOfWeek.Friday));
         }
 
-        [TestMethod]
+        [Fact]
         public void CorrectGarbageCollectionEventsReturned()
         {
             var november2015FridayEvents = new List<GarbageCollectionEvent>
@@ -39,7 +38,7 @@ namespace GreenvilleWiApi.Test
             };
 
             var result = GarbageDayCalculator.CalculateCollectionEvents(DayOfWeek.Friday, new DateTime(2015, 11, 1), new DateTime(2015, 11, 30));
-            CollectionAssert.AreEqual(november2015FridayEvents, result);
+            Assert.Equal(november2015FridayEvents, result);
         }
     }
 }
